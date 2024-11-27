9b251f357aee9bb27182b2ef5acdd356fb62f376
duckie <cedricormond@gmail.com>
2024-11-27 11:55:51 -0600

    implementation of QM state machine. NOT tested

---
+++ src/qm_state_machine.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs.msg import Bool, Int32
+
+from qm_state_machine_impl import QMStateMachine
+
+class QMStateMachineNode:
+
+    def __init__(self):
+        
+
+        self.state_machine = QMStateMachine()
+
+        self.setup_params()
+        self.setup_publishers_and_subscribers()
+        step_timer = rospy.Timer(rospy.Duration(0.1), self.step)
+
+    def step(self, _event):
+        outputs = self.state_machine.step()
+        # TO ASK: 
+        # - I think this may be redundant, as we can simply publish the state of the game in the cb_checkpoint
+        # RESOLVED:I think that done this way is fine, as we can more easily differentiate parts
+        self.pub_game_over.publish(Bool(outputs["game-over"]))
+        self.pub_all_checkpoints_collected.publish(Bool(outputs["game-won"]))
+
+        # TODO: find a way to cleanly stop the game
+    
+    def print_info(self) -> None:
+        print()
+        print()
+        rospy.loginfo(f"Running Quack Man State Machine Node")
+        print()
+        print()
+
+    def setup_params(self) -> None:
+        """
+        Setup the parameters for the node reading them from the ROS parameter server.
+        - self.name_sub_game_state: name of the game state channel
+        - self.name_sub_ghost_bot: name of the channel informing if a GhostBot is seen
+        - self.name_sub_ghost_bot_b: name of the channel informing if the back a GhostBot is seen
+        - self.name_sub_quack_man: name of the channel informing if the QuackMan is seen
+        - self.name_sub_x_sec: name of the channel for detecting x-sec
+        - self.name_pub_lane_following: if set to true the lane following node should start driving
+        - self.name_pub_x_sec_go: if set set to true the x-sec navigation should start navigating through the cross section
+        - self.name_pub_game_over: if set to true the QuackMan was detected and the game is over
+        """
+
+        def get_rosparam(name):
+            if rospy.has_param(name):
+                param = rospy.get_param(name)
+            else:
+                txt_error = f"Parameter '{name}' is not set."
+                rospy.logerr(txt_error)
+                raise KeyError(txt_error)
+            return param
+        
+        # score
+        self.score = 0
+
+        # topics params
+        self.name_sub_game_state = get_rosparam("~topics/sub/game_state")
+        self.name_sub_checkpoint = get_rosparam("~topics/sub/checkpoint")
+
+        self.name_pub_score_updates = get_rosparam("~topics/pub/score_update")
+        self.name_pub_all_checkpoints_collected = get_rosparam("~topics/pub/all_checkpoints_collected")
+        self.name_pub_game_over = get_rosparam("~topics/pub/game_over")
+        
+    def setup_publishers_and_subscribers(self) -> None:
+        """
+        Setup the ROS publishers and subscribers for the node.
+        """
+        self.sub_game_state       = rospy.Subscriber(self.name_sub_game_state,      Bool,   self.state_machine.set_game_state,      queue_size=10)
+        self.sub_checkpoint       = rospy.Subscriber(self.name_sub_checkpoint,      Int32,  self.cb_checkpoint,                     queue_size=10)
+
+        self.pub_score_updates = rospy.Publisher(self.name_pub_score_updates,       Int32,                                          queue_size=10)
+        self.pub_all_checkpoints_collected = rospy.Publisher(self.name_pub_all_checkpoints_collected,       Bool,                   queue_size=10)
+        self.pub_game_over      = rospy.Publisher(self.name_pub_game_over,          Bool,                                           queue_size=10)
+
+
+    def cb_checkpoint(self, msg: int) -> None:
+        self.state_machine.set_cp_status(msg)
+        # Publishe the score, if updated
+        # TOASK: - do we want to publish the score this way, or through the step function?
+        if self.score != self.state_machine.get_score():
+            self.score = self.state_machine.get_score()
+            msg = Int32()
+            msg.data = self.score
+            self.pub_score_updates.publish(msg)
+
+        # # check if all checkpoints were collected
+        # # TOASK: - we don't need to do this here right. We can simply do this above, upon call of the step function
+        # if self.state_machine.all_checkpoints_collected():
+        #     self.pub_all_checkpoints_collected.publish(Bool(True))
+        #     # TODO: find a way to cleanly stop the game
+        #     self.state_machine.set_game_state(False)
+
+    # TODO: need to find a way to publish the game over in case of no checkpoints collected within time frame
+    # RESOLVED: we can simply do this above, upon call of the step function
+
+
+
+
+if __name__ == "__main__":
+    rospy.init_node("qm_state_machine", anonymous=True)
+
+    state_machine = QMStateMachineNode()
+    state_machine.print_info()
+
+    rospy.spin()
---
+++ src/qm_state_machine_impl.py
@@ -0,0 +1,119 @@
+from enum import IntEnum
+import time
+
+class State(IntEnum):
+    IDLE = 0
+    CP_DETECTION = 1
+    GAME_OVER = 2
+    GAME_WON = 3
+
+# TODO: move these to a config file
+TIME_DELTA_CHECKPOINTS = 30
+TOTAL_CHECKPOINTS = 5
+
+
+class QMStateMachine:
+    def __init__(self):
+        # external events
+        self.start: bool = False
+        self.cp_delta_time_elapsed: bool = False
+        self.all_cp_detected: bool = False
+
+        # internal state
+        self.state: State = State.IDLE
+
+
+        # create a set to contain all the identified checkpoints
+        self.checkpoints = set()
+        # player score
+        # right now, simply add 1 for each checkpoint detected
+        self.score = 0
+        
+        # the time at which the last checkpoint was detected
+        # TODO: could initialise as none, and get it to be set upon the first checkpoint detection
+        # currently, this is reset inside the handle_idle function
+        self.last_cp_time = time.time()
+
+
+    def step(self):
+        # update the state, after having delt with the current state's implication and thus its outcome
+        if self.state == State.IDLE:
+            self.state = self.handle_idle()
+        elif self.state == State.CP_DETECTION:
+            self.state = self.handle_cp_detection()
+        elif self.state == State.GAME_OVER:
+            self.state = self.handle_game_over()
+        elif self.state == State.GAME_WON:
+            self.state = self.handle_game_won()
+        return self.get_outputs()
+    
+    def get_outputs(self):
+        # TODO
+        if self.state == State.IDLE:
+            # TOASK: 
+            # - do we want to also publish the score this way, or thorugh the callback, upon checkpoint detection?
+            #   - this would mean we can streamline the cb_checkpoint. 
+            #       But this would mean we mustmake sure that the score is only published when it is updated, 
+            #       and not every time the step function is called, such that the gamemaster is not overwhelmed with messages
+            # - do we want to publish the fact that we are currently in idle or cp_detect state?
+            return{"game-over": False, "game-won": False}#, "score": self.score}
+        if self.state == State.CP_DETECTION:
+            return{"game-over": False, "game-won": False}
+        if self.state == State.GAME_OVER:
+            return{"game-over": True, "game-won": False}
+        if self.state == State.GAME_WON:
+            return{"game-over": False, "game-won": True}
+          
+    def handle_idle(self):
+        next_state = State.IDLE
+        if self.start:
+            self.last_cp_time = time.time()
+            next_state = State.CP_DETECTION
+        return next_state
+    
+    def handle_cp_detection(self):
+        next_state = State.CP_DETECTION
+        if self.cp_delta_time_elapsed:
+            next_state = State.GAME_OVER
+        elif self.all_cp_detected:
+            next_state = State.GAME_WON
+        return next_state
+    
+    def handle_game_over(self):
+        next_state = State.GAME_OVER
+        return next_state  
+    
+    def handle_game_won(self):
+        next_state = State.GAME_WON
+        return next_state  
+    
+    def set_game_state(self, val: int) -> None:
+        self.start = val == 1
+
+    # TODO: 
+    # - if we change the message type to stamped int and bool, we could use 
+    # the message time, to evaluate at what time the checkpoint was detected
+    # TOASK: 
+    # - baseically, this function gets called everytime a checkpoint gets publisehd. 
+    # Isn't there a risk that it get soverwhlemed and called too many times, 
+    # preventing the rest from occuring?
+    # - we should maybe make sure that the checkpoints are not published at a too high frequency, 
+    # or woudl this not be a problem?
+    def set_cp_status(self, val: int) -> None:
+        curr_time = time.time()
+        # check if the time elapsed since the last checkpoint detection is greater than the threshold
+        if curr_time - self.last_cp_time > TIME_DELTA_CHECKPOINTS:
+            self.cp_delta_time_elapsed = True    
+        # discovered a new checkpoint, within the time window
+        elif val not in self.checkpoints:
+            self.score += 1
+            self.last_cp_time = curr_time
+            self.checkpoints.add(val)
+
+            if len(self.checkpoints) == TOTAL_CHECKPOINTS:
+                self.all_cp_detected = True
+            # redundant, would never occur
+            # self.cp_delta_time_elapsed = False
+    
+    def get_score(self) -> int:
+        return self.score
